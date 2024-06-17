2ccc6c57f25178118cef6a4559f27c3aedf102b5
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-06-17 23:58:04 +0200

    updated server file

--- backend-riux/index.js
+++ backend-riux/index.js
@@ -1,28 +1,25 @@
 // backend/index.js
 const express = require('express');
 const cors = require('cors');
-const sequelize = require('./config/database');
-const authRoutes = require('./routes/authRoutes');
+const sequelize = require('./config/database'); 
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', userRoutes);
 
-// Test Database Connection and Start Server
+const PORT = process.env.PORT || 5000;
+
 sequelize.authenticate()
     .then(() => {
         console.log('Database connected...');
-        return sequelize.sync();
-    })
-    .then(() => {
-        app.listen(5000, () => {
-            console.log('Server running on port 5000');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         });
     })
-    .catch(err => console.log('Error: ' + err));
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
