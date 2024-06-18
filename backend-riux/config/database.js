3473fa31ecf7a9508c1b429c485d732f21c3fae2
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-06-18 09:05:25 +0200

    commented out code

--- backend-riux/config/database.js
+++ backend-riux/config/database.js
@@ -1,16 +1,16 @@
-// backend/config/database.js
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+// // backend/config/database.js
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+// });
 
-sequelize.sync().then(() => {
-    console.log('Database synchronized');
-}).catch((error) => {
-    console.error('Error synchronizing database:', error);
-});
+// sequelize.sync().then(() => {
+//     console.log('Database synchronized');
+// }).catch((error) => {
+//     console.error('Error synchronizing database:', error);
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
--- backend-riux/index.js
+++ backend-riux/index.js
@@ -1,25 +1,25 @@
-// backend/index.js
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/database'); 
-const userRoutes = require('./routes/userRoutes');
-require('dotenv').config();
+// // backend/index.js
+// const express = require('express');
+// const cors = require('cors');
+// const sequelize = require('./config/database'); 
+// const userRoutes = require('./routes/userRoutes');
+// require('dotenv').config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-app.use('/api/auth', userRoutes);
+// app.use('/api/auth', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Database connected...');
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Database connected...');
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         });
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
